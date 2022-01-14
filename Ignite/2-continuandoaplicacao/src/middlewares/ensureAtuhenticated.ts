59380e92f2c7eb7a7c6d22337c616213a33148ca
Gabriel Fernella <gabrielfernella07@hotmail.com>
2022-01-13 23:50:13 -0300

    Create middleware token

---
+++ Ignite/2-continuandoaplicacao/src/middlewares/ensureAtuhenticated.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+// cria uma interface para o token
+interface IPayload {
+    sub: string;
+}
+
+export async function ensureAuthenticated(
+    request: Request,
+    response: Response,
+    next: NextFunction
+): Promise<void> {
+    // propriedade padrão para o JWT
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        throw new Error("Token missing");
+    }
+
+    const [, token] = authHeader.split(" ");
+
+    try {
+        const { sub: user_id } = verify(
+            token,
+            "958078df2cab766c85ee67ce5ccb2428"
+        ) as IPayload;
+
+        const usersRepository = new UsersRepository();
+        const user = usersRepository.findById(user_id);
+
+        if (!user) {
+            throw new Error("User does not exists");
+        }
+
+        next();
+    } catch (error) {
+        throw new Error("Invalid Token");
+    }
+}
