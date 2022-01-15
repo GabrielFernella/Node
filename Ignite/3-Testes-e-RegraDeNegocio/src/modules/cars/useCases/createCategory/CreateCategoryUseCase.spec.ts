db73b2d78a146a7d371e61516c6f00f6d17e450e
Gabriel Fernella <gabrielfernella07@hotmail.com>
2022-01-15 03:27:10 -0300

    first test example

--- Ignite/3-Testes-e-RegraDeNegocio/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ Ignite/3-Testes-e-RegraDeNegocio/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,13 +1,59 @@
-describe("Criar categoria", () => {
-    it("Espero que 2+2 seja 4", () => {
-        const soma = 2 + 2;
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-        expect(soma).toBe(4);
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe("Create category", () => {
+    beforeEach(() => {
+        // Antes de qualquer teste, é realizado essas instancias para que vc consiga usar o respository
+        categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+        createCategoryUseCase = new CreateCategoryUseCase(
+            categoriesRepositoryInMemory
+        );
     });
 
-    it("Espero que 2+2 seja 4", () => {
-        const soma = 2 + 2;
+    it("should be able to create a new Category", async () => {
+        // Criando um Objeto para utilziar os dados
+        const category = {
+            name: "Category Test",
+            description: "Category description test",
+        };
 
-        expect(soma).not.toBe(5);
+        // Chamando o serviço useCase para testar e passando os parâmetros
+        await createCategoryUseCase.execute({
+            name: category.name,
+            description: category.description,
+        });
+
+        // valor que vamos pegar para conseguir testar
+        const categoryCreated = await categoriesRepositoryInMemory.findByName(
+            category.name
+        );
+
+        // Esse expect espera que a variável possua um valor em category.id
+        expect(categoryCreated).toHaveProperty("id");
+    });
+
+    it("should not be able to create a new Category with name exists", async () => {
+        // Nesse caso colocamos a função dentro do expect para que ela de fato de erro no nosso useCase e assim
+        // pegamos o reject e identificamos que teve uma instancia do APPError
+        expect(async () => {
+            const category = {
+                name: "Category Test",
+                description: "Category description test",
+            };
+
+            await createCategoryUseCase.execute({
+                name: category.name,
+                description: category.description,
+            });
+
+            await createCategoryUseCase.execute({
+                name: category.name,
+                description: category.description,
+            });
+        }).rejects.toBeInstanceOf(AppError);
     });
 });
